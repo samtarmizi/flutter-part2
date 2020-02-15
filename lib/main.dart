25157ce341dcbb9b2693a665863ec0a869012940
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-02-15 14:41:28 +0800

    add image from internet

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyFormPage(),
@@ -24,8 +33,22 @@ class MyFormPage extends StatefulWidget {
 class _MyFormPageState extends State<MyFormPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Form"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Text("Form app"),
+            Image.network("https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg")
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.mail),
+        onPressed: () {},
+      ),
     );
   }
 }
