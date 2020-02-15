581783e36e86b334895829e7b2c066d101028e28
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-02-15 16:29:02 +0800

    define variable to accept data

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatefulWidget {
+
+  final String name;
+  final String email;
+  final String phone;
+
+  const DetailPage({Key key, this.name, this.email, this.phone}) : super(key: key);
+
   @override
   _DetailPageState createState() => _DetailPageState();
 }
 
 class _DetailPageState extends State<DetailPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
